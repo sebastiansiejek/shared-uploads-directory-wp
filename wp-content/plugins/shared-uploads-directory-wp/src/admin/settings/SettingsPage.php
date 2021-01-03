893c5f39baa1aaad599e46bd4cbb7e5ad984e220
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-01-03 19:50:20 +0100

    feat: add SettingsPage

---
+++ wp-content/plugins/shared-uploads-directory-wp/src/admin/settings/SettingsPage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SharedUploadsDirectoryPlugin\src\admin\settings;
+
+class SettingsPage
+{
+
+  function add()
+  {
+    add_menu_page(
+      'Shared Uploads Directory',
+      'Shared Uploads Directory Options',
+      'manage_options',
+      Settings::slug,
+      function () {
+?>
+      <form action="options.php" method="post">
+        <?php
+        settings_fields(Settings::slug . "_options");
+        do_settings_sections(Settings::slug);
+        submit_button(__('Save'));
+        ?>
+      </form>
+<?php
+      }
+    );
+  }
+}
