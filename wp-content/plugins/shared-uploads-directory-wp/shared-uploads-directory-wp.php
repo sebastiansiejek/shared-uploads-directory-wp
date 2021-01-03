34565ab415854eae887f84ab3ec5aee7ed08d11c
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-01-03 13:53:07 +0100

    feat: add init file with header

---
+++ wp-content/plugins/shared-uploads-directory-wp/shared-uploads-directory-wp.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       Shared Uploads Directory Plugin
+ * Plugin URI:        https://github.com/sebastiansiejek/shared-uploads-directory-wp
+ * Description:       Plugin keeps /uploads directory on remote server.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Sebastian Siejek
+ * Author URI:        https://sebastiansiejek.dev
+ * Text Domain:       shared-uploads-directory-wp
+ * Domain Path:       /languages
+ */
