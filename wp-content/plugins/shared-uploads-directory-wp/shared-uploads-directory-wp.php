65b8180b65ead7f33a22a54c02d0ad0326886eac
Sebastian Siejek <s.siejek@empressia.pl>
2021-12-29 14:23:48 +0100

    chore: set 1.0.0v

--- wp-content/plugins/shared-uploads-directory-wp/shared-uploads-directory-wp.php
+++ wp-content/plugins/shared-uploads-directory-wp/shared-uploads-directory-wp.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Shared Uploads Directory Plugin
  * Plugin URI:        https://github.com/sebastiansiejek/shared-uploads-directory-wp
  * Description:       Plugin keeps /uploads directory on remote server.
- * Version:           0.0.1
+ * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            Sebastian Siejek
